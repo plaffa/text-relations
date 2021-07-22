07ea87d2f8c59fc04884857d457812fae05e9aea
Patrick Gorton <ragaedude@gmail.com>
2021-07-22 17:32:59 +0200

    finished for now

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--visualisation_method', type=str, default='tsne')
 parser.add_argument('--file_directory', type=str, default='text_files')
 parser.add_argument('--max_pca_components', type=int, default=100)
-parser.add_argument('--tsne_perplexity', type=float, default=7.5)
+parser.add_argument('--tsne_perplexity', type=float, default=7.0)
 args = parser.parse_args()
 
 if __name__ == "__main__":
--- visualisation.py
+++
@@ -1,7 +0,0 @@
-
-import sklearn.manifold._t_sne as tsne
-
-class Visualiser:
-    def __init__(self):
-        pass
-    pass
